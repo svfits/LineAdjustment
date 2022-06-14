db6165e7a834bf268ce1552d5ca38297d5582a37
Дмитрий Афанасьев <dafanasiev@dengisrazy.ru>
2022-06-14 09:04:52 +0800

    Бизнес логика. Нужен рефакторинг

--- LineAdjustment/LineAdjustmentAlgorithm.cs
+++ LineAdjustment/LineAdjustmentAlgorithm.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace LineAdjustment
 {
@@ -6,7 +9,118 @@ namespace LineAdjustment
     {
         public string Transform(string input, int lineWidth)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            var arStr = new ReadOnlySpan<string>(input.Split(' '));
+            var resultStr = new StringBuilder(capacity: lineWidth);
+
+            if (arStr.Length == 1)
+            {
+                resultStr.Append(arStr[0]);
+                for (int i = 0; i < (lineWidth - arStr[0].Length); i++)
+                {
+                    resultStr.Append(' ');
+                }
+                return resultStr.ToString();
+            }
+
+            var resultList = new List<string>(capacity: arStr.Length);
+            var currentStrArray = new List<string>();
+            foreach (var item in arStr)
+            {
+                string v1 = string.Join(" ", currentStrArray);
+                int currentStrlen = v1.Length;
+                var sumLength = currentStrlen + item.Length;
+
+                if (sumLength >= lineWidth)
+                {
+                    int v = (lineWidth - currentStrlen);
+
+                    if (currentStrArray.Count == 1)
+                    {
+                        var word = new StringBuilder(currentStrArray[0]);
+
+                        for (int j = 0; j < v; j++)
+                        {
+                            word.Append(' ');
+                        }
+
+                        currentStrArray[0] = word.ToString();
+                    }
+                    else
+                    {
+                        for (int i = 0; i < currentStrArray.Count; i++)
+                        {
+                            var word = new StringBuilder(currentStrArray[i]);
+                            if (i == 0)
+                            {
+                                for (int j = 0; j <= v; j++)
+                                {
+                                    word.Append(' ');
+                                }
+
+                                currentStrArray[0] = word.ToString();
+                                continue;
+                            }
+
+                            currentStrArray[i] = word.Append(' ').ToString();
+                        }
+                    }
+
+                    var str = String.Join("", currentStrArray);
+
+                    if (currentStrArray.Count != 1)
+                    {
+                        str = str.TrimEnd();
+                    }
+
+                    System.Diagnostics.Debug.Write(str);
+                    System.Diagnostics.Debug.WriteLine(str.Length);
+
+                    resultList.Add(str);
+
+                    currentStrArray.Clear();
+                    currentStrArray.Add(item);
+                }
+                else
+                {
+                    currentStrArray.Add(item);
+                }
+            }
+
+            System.Diagnostics.Debug.Write(currentStrArray.Last());
+
+            if (currentStrArray.Any())
+            {
+                string v1 = string.Join(" ", currentStrArray);
+                int currentStrlen = v1.Length;
+                int v = (lineWidth - currentStrlen);
+
+                for (int i = 0; i < currentStrArray.Count; i++)
+                {
+                    var word = new StringBuilder(currentStrArray[i]);
+                    if (i == 0)
+                    {
+                        for (int j = 0; j < v; j++)
+                        {
+                            word.Append(' ');
+                        }
+
+                        currentStrArray[0] = word.ToString();
+                        continue;
+                    }
+
+                    currentStrArray[i] = word.Append(' ').ToString();
+                }
+
+                var str = String.Join("", currentStrArray);
+                resultList.Add(str);
+            }
+
+            return String.Join("\n", resultList);
         }
     }
 }
